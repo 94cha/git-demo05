8f2485c0bfe71db5c4349009dba5ba7b332c9eae
xiaocha <623081550@qq.com>
2020-09-19 18:18:02 +0800

    ojbkkkk

---
+++ src/main/java/com/cha/git/GitDemo022Application.java
@@ -0,0 +1,13 @@
+package com.cha.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemo022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cha/git/GitDemo022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cha.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
